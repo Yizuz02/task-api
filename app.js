20c8f1e34e9e2316dd9a2f3aa8b56ebd1462428d
Yizuz02 <jchirris04@gmail.com>
2026-07-17 22:08:26 -0600

    Stage 3: create with validation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+
 const port = 3000;
+app.use(express.json());
 
 const tasks = [
   {id: 1, title: "Study JavaScript", done: false},
@@ -34,6 +36,19 @@ app.get('/tasks/:id', (req, res) => {
   } else {
     res.status(404).send({ "error": `Task ${id} not found` });
   }
+});
+
+app.post('/tasks', (req, res) => {
+  const task = req.body;
+
+  if (!task.title || task.title.trim() === ""){
+    return res.status(400).send({ "error": "Title is required" });
+  }
+  
+  const newId = Math.max(...tasks.map(task => task.id)) + 1;
+  const newTask = {id: newId, title: task.title, done: false};
+  tasks.push(newTask);
+  res.status(201).send(newTask);
 });
 
 app.listen(port, () => {
