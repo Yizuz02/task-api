aa9df360fd7ddf3ce6c13eae090a1a0a4d0eff5b
Yizuz02 <jchirris04@gmail.com>
2026-07-17 22:49:04 -0600

    Stage 4: full CRUD

--- app.js
+++ app.js
@@ -41,14 +41,58 @@ app.get('/tasks/:id', (req, res) => {
 app.post('/tasks', (req, res) => {
   const task = req.body;
 
-  if (!task.title || task.title.trim() === ""){
+  if (!task.title){
     return res.status(400).send({ "error": "Title is required" });
   }
-  
+  if (typeof task.title !== "string" || task.title.trim() === ""){
+    return res.status(400).send({ "error": "Invalid title" });
+  }
+
   const newId = Math.max(...tasks.map(task => task.id)) + 1;
   const newTask = {id: newId, title: task.title, done: false};
   tasks.push(newTask);
   res.status(201).send(newTask);
+});
+
+app.put('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const foundTaskIndex = tasks.findIndex(task => task.id === Number(id))
+
+  if (foundTaskIndex === -1){
+    return res.status(404).send({ "error": `Task ${id} not found` });
+  }
+
+  const body = req.body;
+
+  if (!("title" in body) && !("done" in body)){
+    return res.status(400).send({ "error": "Title or done status is required" });
+  }
+
+  if ("title" in body){
+    if (typeof body.title !== "string" || body.title.trim() === ""){
+      return res.status(400).send({ "error": "Invalid title" });
+    }
+    tasks[foundTaskIndex].title = body.title
+  }
+
+  if ("done" in body){
+    if (typeof body.done !== "boolean"){
+      return res.status(400).send({ "error": "Invalid done status" });
+    }
+    tasks[foundTaskIndex].done = body.done
+  }
+  
+  res.send(tasks[foundTaskIndex]);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const foundTaskIndex = tasks.findIndex(task => task.id === Number(id));
+  if (foundTaskIndex === -1){
+    return res.status(404).send({ "error": `Task ${id} not found` });
+  }
+  tasks.splice(foundTaskIndex, 1)
+  res.status(204).send();
 });
 
 app.listen(port, () => {
