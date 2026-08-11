9ae98736a1236ada50eadfa3cdb0f18cfa4f642e
Yizuz02 <jchirris04@gmail.com>
2026-08-11 17:23:13 -0600

    Stage 0: create SQLite database

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const Database = require('better-sqlite3');
+
+const db = new Database('tasks.db');
+
 const express = require('express');
 const app = express();
 
@@ -7,6 +11,37 @@ const swaggerDocument = require('./openapi.json');
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        done INT NOT NULL DEFAULT 0
+    )
+`);
+
+const insertTask = db.prepare(`
+    INSERT INTO tasks (title, done)
+    VALUES (@title, @done)
+`);
+
+const insertManyTask = db.transaction((tasks) => {
+  for (const task of tasks) insertTask.run(task);
+});
+
+const numRows = db.prepare('SELECT COUNT(*) as count FROM tasks;').get().count;
+
+
+
+if (numRows === 0){
+  insertManyTask([
+    {title: "Study JavaScript", done: 0},
+    {title: "Develop To-Do list API for Intership", done: 0},
+    {title: "Buy groceries", done: 1}
+  ]);
+}
+
+
 
 const originalTasks = [
   {id: 1, title: "Study JavaScript", done: false},
